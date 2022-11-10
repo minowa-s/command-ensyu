e48c8b5f9f69b33653387128936acfec4a6062b9
minowa <s.minowa.sys22@morijyobi.ac.jp>
2022-11-10 10:53:53 +0900

    １回の実行で２人分作成できるように編集。

--- Main.java
+++ Main.java
@@ -4,17 +4,28 @@ import java.util.Scanner;
 
 public class Main{
 		public static void main(String[] args){
-	        Scanner sc =new Scanner(System.in);
-			BigDecimal bd;
+    Scanner sc =new Scanner(System.in);
+    BigDecimal bd;
+    BigDecimal bd2;
 
-    System.out.println("身長を入力してください。");
+    System.out.println("1人目の身長を入力してください。");
     double m = sc.nextDouble();
-    System.out.println("体重を入力してください。");
+    System.out.println("1人目の体重を入力してください。");
     double  kg = sc.nextDouble();
+    System.out.println("2人目の身長を入力してください。");
+    double m2 = sc.nextDouble();
+    System.out.println("2人目の体重を入力してください。");
+    double kg2 = sc.nextDouble();
 
     double bmi = kg / Math.pow(m, 2);
     bd = new BigDecimal(bmi);
     bd = bd.setScale(2,RoundingMode.HALF_UP);
-    System.out.println("bmiは"+bd+"です。");	
+    
+    double bmi2 = kg2 / Math.pow(m2, 2);
+    bd2 = new BigDecimal(bmi2);
+    bd2 = bd.setScale(2,RoundingMode.HALF_UP);
+    System.out.println("1人目のBMIは"+bd+"です。");
+    System.out.println("2人目のBMIは"+bd2+"です。");	        
+	
 		}
 }		
